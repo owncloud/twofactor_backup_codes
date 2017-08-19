635023837639a07abcad753a690e4eb1448596fe
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-19 02:10:14 +0000

    [tx-robot] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "You can generate second factor backup codes below." : "ניתן ליצור קוד גיבוי מסדר שני מטה.",
+    "Generate codes" : "יצירת קודים",
+    "You have {{remaining}} backup codes left can be used." : "נותרו ברשותך {{remaining}} קודי גיבוי לשימוש.",
+    "Regenerate codes" : "יצירת קודים מחדש",
+    "Backup Codes" : "קודי גיבוי",
+    "Authenticate with a backup code" : "אימות על בסיס קוד גיבוי",
+    "Second-factor backup codes" : "קודי גיבוי מסדר שני"
+},
+"nplurals=2; plural=(n != 1);");
