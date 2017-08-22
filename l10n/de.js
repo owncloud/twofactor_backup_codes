7c1c8d6d727ec871b2624b8249f5e0f7ddaeae64
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-22 02:10:17 +0000

    [tx-robot] updated from transifex

---
+++ l10n/de.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "You can generate second factor backup codes below." : "Du kannst unten die Sicherungscodes des zweiten Faktors generieren.",
+    "Generate codes" : "Codes generieren",
+    "You have {{remaining}} backup codes left can be used." : "Du hast {{remaining}} Sicherungscodes zur weiteren Verwendung übrig.",
+    "Regenerate codes" : "Codes regenerieren",
+    "Backup Codes" : "Sicherungscodes",
+    "Authenticate with a backup code" : "Mit einem Sicherungscode authentisieren",
+    "Second-factor backup codes" : "Sicherungscodes für den zweiten Faktor"
+},
+"nplurals=2; plural=(n != 1);");
