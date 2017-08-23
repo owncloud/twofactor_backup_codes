ce03c31cf534821981e9b2aa2dfb20458cfa01d6
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-23 02:10:17 +0000

    [tx-robot] updated from transifex

---
+++ l10n/th_TH.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "You can generate second factor backup codes below." : "คุณสามารถสร้างรหัสสำรองที่สองด้านล่าง",
+    "Generate codes" : "สร้างรหัส",
+    "You have {{remaining}} backup codes left can be used." : "คุณมีรหัสสำรองเหลือแค่ {{remaining}}  รหัสที่สามารถใช้ได้",
+    "Regenerate codes" : "สร้างรหัสใหม่",
+    "Backup Codes" : "รหัสสำรองข้อมูล",
+    "Authenticate with a backup code" : "ตรวจสอบสิทธิ์ด้วยรหัสสำรอง",
+    "Second-factor backup codes" : "รหัสสำรองที่สอง"
+},
+"nplurals=1; plural=0;");
