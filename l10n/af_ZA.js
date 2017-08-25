7e2e3acc096c304bc5a8f02785def19ce02c5a07
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-25 02:10:18 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "You can generate second factor backup codes below." : "U kan hier onder tweedefaktorrugsteunkodes genereer.",
+    "Generate codes" : "Generee kodes",
+    "You have {{remaining}} backup codes left can be used." : "U het nog {{remaining}} kodes oor vir gebruik.",
+    "Regenerate codes" : "Hergenereer kodes",
+    "Backup Codes" : "Rugsteunkodes",
+    "Authenticate with a backup code" : "Verifieer met ’n rugsteunkode",
+    "Second-factor backup codes" : "Tweedefaktorrugsteunkodes"
+},
+"nplurals=2; plural=(n != 1);");
