0b3f82d472cd78402f4f6e3eb6f5998c89b7b67f
ownCloud Jenkins <jenkins@owncloud.com>
2017-09-14 02:10:19 +0000

    [tx-robot] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -1,8 +1,10 @@
 OC.L10N.register(
     "twofactor_backup_codes",
     {
+    "Backup codes let you access your account if your other factors are not available." : "קודים מגובים מאפשרים לך להיכנס לחשבון אם שאר האמצעים אינם זמינים.",
     "You can generate second factor backup codes below." : "ניתן ליצור קוד גיבוי מסדר שני מטה.",
     "Generate codes" : "יצירת קודים",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "יש לשמור עליהם במקום נגיש, כמו הארנק שלך. ניתן להשתמש בכל קוד פעם אחת בלבד.",
     "You have {{remaining}} backup codes left can be used." : "נותרו ברשותך {{remaining}} קודי גיבוי לשימוש.",
     "Regenerate codes" : "יצירת קודים מחדש",
     "Backup Codes" : "קודי גיבוי",
