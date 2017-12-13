912890cdc96bb65f6e01a43efba728aec4d58461
ownCloud Jenkins <jenkins@owncloud.com>
2017-12-13 03:15:36 +0000

    [tx-robot] updated from transifex

---
+++ l10n/sq.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "Backup codes let you access your account if your other factors are not available." : "Kodet e kopjeruajtjeve ju lejon të hyni në llogarinë tuaj nëse s’ka faktorë të tjerë.",
+    "You can generate second factor backup codes below." : "Më poshtë mund të prodhoni kode kopjeruajtjesh faktori të dytë.",
+    "Generate codes" : "Prodho kode",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "Ruajini diku ku mund të merrni lehtë, fjala vjen në portofolin tuaj. Çdo kod mund të përdoret vetëm një herë.",
+    "You have {{remaining}} backup codes left can be used." : "Keni ende {{remaining}} kode kopjeruajtje që mund të përdorni.",
+    "Regenerate codes" : "Riprodho kode",
+    "Backup Codes" : "Kode Kopjeruajtjesh",
+    "Authenticate with a backup code" : "Kryej mirëfilltësim me një kod kopjeruajtjeje",
+    "Second-factor backup codes" : "Kode kopjeruajtjesh me faktor të dytë"
+},
+"nplurals=2; plural=(n != 1);");
