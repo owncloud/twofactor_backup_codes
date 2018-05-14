5654bf21dfa85107d5b4c29f56aaa1cde73131c2
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-14 02:10:23 +0000

    [tx-robot] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -1,8 +1,10 @@
 OC.L10N.register(
     "twofactor_backup_codes",
     {
+    "Backup codes let you access your account if your other factors are not available." : "Kurtarma kodları diğer ikincil yöntemleriniz ulaşılabilir olmadığında, hesabınıza erişmenize izin verir.",
     "You can generate second factor backup codes below." : "İkincil yöntem kurtarma kodlarınızı aşağıdan üretebilirsiniz.",
     "Generate codes" : "Kodları üret",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "Kodları cüzdanınız gibi ulaşılabilir bir yerde saklayınız. Her kod sadece bir kere kullanılabilir.",
     "You have {{remaining}} backup codes left can be used." : "{{remaining}} adet kullanılabilecek kurtarma kodunuz kaldı.",
     "Regenerate codes" : "Kodları yeniden üret",
     "Backup Codes" : "Kurtarma Kodları",
