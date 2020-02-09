6bd65311605c13d99a1d673512f9ec0ac5098e0f
Dominik Schmidt <domme@tomahawk-player.org>
2020-02-09 02:27:43 +0000

    [tx] updated from transifex

---
+++ l10n/bg_BG.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "Backup codes let you access your account if your other factors are not available." : "Резервните кодове ви позволяват да получите достъп до вашия акаунт, ако другите фактори не са налични.",
+    "You can generate second factor backup codes below." : "Можете по-долу да генерирате резервни кодове за втори фактор.",
+    "Generate codes" : "Генериране на кодове",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "Дръжте ги някъде където да са достъпни, като портфейла ви. Всеки код може да се използва само веднъж.",
+    "You have {{remaining}} backup codes left can be used." : "Имате {{remaining}} резервни кодове, които могат да се използват.",
+    "Regenerate codes" : "Обновяване на кодове",
+    "Backup Codes" : "Резервни кодове",
+    "Authenticate with a backup code" : "Удостоверяване с резервен код",
+    "Second-factor backup codes" : "Втори-фактор резервни кодове"
+},
+"nplurals=2; plural=(n != 1);");
