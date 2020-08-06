e485b05782ac3d736a4e41f3df896fb307fdc7f8
Thomas Müller <thomas.mueller@tmit.eu>
2020-08-06 01:36:26 +0000

    [tx] updated from transifex

--- l10n/ar.js
+++ l10n/ar.js
@@ -1,8 +1,10 @@
 OC.L10N.register(
     "twofactor_backup_codes",
     {
+    "Backup codes let you access your account if your other factors are not available." : "تتيح لك الرموز الاحتياطية الوصول إلى حسابك إذا كانت عواملك الأخرى غير متوفرة.",
     "You can generate second factor backup codes below." : "يمكنك توليد عامل رموز إحتياطية ثان أدناه.",
     "Generate codes" : "توليد شفرات",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "احتفظ بها في مكان يسهل الوصول إليه ، مثل محفظتك. يمكن استخدام كل رمز مرة واحدة فقط.",
     "You have {{remaining}} backup codes left can be used." : "تبقى لديك {{remaining}} رموز إحتياطية يمكنك استعمالها.",
     "Regenerate codes" : "إعادة توليد شفرات",
     "Backup Codes" : "رموز إحتياطية",
