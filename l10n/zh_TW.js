05d1e41353e2c23100ffe42ae9a947e456dff092
Thomas Müller <thomas.mueller@tmit.eu>
2020-09-28 01:57:16 +0000

    [tx] updated from transifex

---
+++ l10n/zh_TW.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "Backup codes let you access your account if your other factors are not available." : "如果您的其他因素不可，可用的用備份驗證碼訪問您的帳號。",
+    "You can generate second factor backup codes below." : "您可以在下面生成第二因素備份代碼。",
+    "Generate codes" : "產生驗證碼",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "將它們放在可取放的地方，例如錢包。 每個代碼只能使用一次。",
+    "You have {{remaining}} backup codes left can be used." : "您有 {{remaining}} 備份可以使用剩下的代碼。",
+    "Regenerate codes" : "重新生成代碼",
+    "Backup Codes" : "備份代號",
+    "Authenticate with a backup code" : "驗證與備份碼",
+    "Second-factor backup codes" : "次要備份碼"
+},
+"nplurals=1; plural=0;");
