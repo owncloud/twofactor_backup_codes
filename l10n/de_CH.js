a582b6aa782d1a6983e66bf0b0562e7b89bd9c34
Thomas Müller <thomas.mueller@tmit.eu>
2020-11-01 00:05:42 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "Backup codes let you access your account if your other factors are not available." : "Sicherungscodes lässen dich auf dein Konto zugreifen, wenn deine anderen Zugriffsmöglichkeiten nicht verfügbar sind.",
+    "You can generate second factor backup codes below." : "Du kannst unten die Sicherungscodes des zweiten Faktors generieren.",
+    "Generate codes" : "Codes generieren",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "Bewahre diese irgendwo zugänglich auf, wie z.B. deine Brieftasche. Jeder Code kann nur einmal verwendet werden.",
+    "You have {{remaining}} backup codes left can be used." : "Du hast {{remaining}} Sicherungscodes zur weiteren Verwendung übrig.",
+    "Regenerate codes" : "Codes regenerieren",
+    "Backup Codes" : "Sicherungscodes",
+    "Authenticate with a backup code" : "Mit einem Sicherungscode authentisieren",
+    "Second-factor backup codes" : "Sicherungscodes für den zweiten Faktor"
+},
+"nplurals=2; plural=(n != 1);");
