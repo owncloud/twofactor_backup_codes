c2497b3bf953ee005eb8e0cfe47dcc8c1c6111fe
ownClouders <devops@owncloud.com>
2024-10-14 01:39:03 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "twofactor_backup_codes",
+    {
+    "Backup codes let you access your account if your other factors are not available." : "زاپاسلاش كودى باشقا ئامىللىرىڭىز بولمىسا ھېساباتىڭىزغا كىرەلەيدۇ.",
+    "You can generate second factor backup codes below." : "تۆۋەندىكى ئىككىنچى ئامىل زاپاسلاش كودىنى ھاسىل قىلالايسىز.",
+    "Generate codes" : "كود ھاسىل قىلىڭ",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "ھەميانغا ئوخشاش ئۇلارنى زىيارەت قىلغىلى بولىدىغان بىر جايدا ساقلاڭ. ھەر بىر كودنى پەقەت بىرلا قېتىم ئىشلىتىشكە بولىدۇ.",
+    "You have {{remaining}} backup codes left can be used." : "سىزدە {{قالغان}} زاپاسلاش كودى قالدى.",
+    "Regenerate codes" : "كودلارنى قايتا ھاسىل قىلىڭ",
+    "Remove codes" : "كودلارنى ئۆچۈرۈڭ",
+    "Backup Codes" : "زاپاسلاش كودى",
+    "Authenticate with a backup code" : "زاپاسلاش كودى بىلەن دەلىللەڭ",
+    "Verify" : "دەلىللەڭ",
+    "Second-factor backup codes" : "ئىككىنچى ئامىل زاپاسلاش كودى"
+},
+"nplurals=2; plural=(n != 1);");
