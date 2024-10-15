d41b3a3b162a2a8893539c477a7db9d3411bdc52
ownClouders <devops@owncloud.com>
2024-10-15 00:10:52 +0000

    [tx] updated from transifex

--- l10n/nl.js
+++ l10n/nl.js
@@ -1,12 +1,14 @@
 OC.L10N.register(
     "twofactor_backup_codes",
     {
+    "Backup codes let you access your account if your other factors are not available." : "Met back-upcodes heb je toegang tot je account als je andere factoren niet beschikbaar zijn.",
     "You can generate second factor backup codes below." : "Je kunt hieronder Tweede Factor back-up codes genereren.",
     "Generate codes" : "Genereer codes",
     "You have {{remaining}} backup codes left can be used." : "Je hebt {{remaining}} back-up codes over.",
     "Regenerate codes" : "Opnieuw genereren codes",
     "Backup Codes" : "Back-up codes",
     "Authenticate with a backup code" : "Authenticeren met een back-up code",
+    "Verify" : "Verifieer",
     "Second-factor backup codes" : "Tweede Factor back-up codes"
 },
 "nplurals=2; plural=(n != 1);");
