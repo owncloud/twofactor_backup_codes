b028bed78cd0c534e814608d49f6ee7b274dffaf
ownClouders <devops@owncloud.com>
2025-12-02 00:07:28 +0000

    [tx] updated from transifex

--- l10n/es.js
+++ l10n/es.js
@@ -1,10 +1,10 @@
 OC.L10N.register(
     "twofactor_backup_codes",
     {
-    "Backup codes let you access your account if your other factors are not available." : "Los códigos de Backup permiten el acceso a tu cuenta si los otros factores no están disponibles.",
+    "Backup codes let you access your account if your other factors are not available." : "Los códigos de respaldo permiten el acceso a tu cuenta si los otros factores no están disponibles.",
     "You can generate second factor backup codes below." : "Debajo puedes generar códigos de respaldo para doble factor de identificación.",
     "Generate codes" : "Códigos generados",
-    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "Guardar en algún sitio accesible, como su monedero. Cada código solo puede ser usado una vez.",
+    "Keep them somewhere accessible, like your wallet. Each code can be used only once." : "Guárdelos en algún sitio accesible, como su monedero. Cada código solo puede usarse una vez.",
     "You have {{remaining}} backup codes left can be used." : "Tienes {{remaining}} códigos de respaldo libres listos para usar.",
     "Regenerate codes" : "Regenerar códigos",
     "Remove codes" : "Eliminar códigos",
